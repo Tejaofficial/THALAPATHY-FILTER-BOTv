70f030735887c58d518f7594f5f7152d6a20405f
Tejaofficial <162498148+Tejaofficial@users.noreply.github.com>
2025-05-19 13:17:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Chowdary106/THALAPATHY-FILTER-BOTv /THALAPATHY-FILTER-BOT 
+  git clone https://github.com/Tejaofficial/THALAPATHY-FILTER-BOTv /THALAPATHY-FILTER-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
